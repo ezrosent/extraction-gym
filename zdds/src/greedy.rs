f08c071718e9f3b3392edd8b0634bd4d5b0d2cc2
Eli Rosenthal <ezr@cs.brown.edu>
2023-07-17 00:28:36 -0700

    Refactor costsummary api

--- zdds/src/greedy.rs
+++ zdds/src/greedy.rs
@@ -16,21 +16,34 @@ type GreedyTree<'a, E> = FixedPointSolver<'a, E, TreeCost>;
 #[allow(type_alias_bounds)]
 type GreedyDag<'a, E: Egraph> = FixedPointSolver<'a, E, DagCost<<E as Egraph>::ENodeId>>;
 
-pub(crate) type AnalysisResult<'a, E, C> = HashMap<
-    <E as Egraph>::EClassId,
-    NodeState<<E as Egraph>::ENodeId, Wrapper<C>, FixedPointSolver<'a, E, C>>,
->;
+pub(crate) type AnalysisResult<'a, E, C> =
+    HashMap<<E as Egraph>::EClassId, NodeState<<E as Egraph>::ENodeId, Wrapper<C>>>;
 
-pub(crate) struct FixedPointSolver<'a, E: Egraph, C: CostSummary<Self, E::ENodeId>> {
-    class_guesses: HashMap<E::EClassId, NodeState<E::ENodeId, Wrapper<C>, Self>>,
-    costs: Rc<HashMap<E::ENodeId, Cost>>,
+pub(crate) struct FixedPointSolver<'a, E: Egraph, C: CostSummary<E::ENodeId>> {
+    class_guesses: HashMap<E::EClassId, NodeState<E::ENodeId, Wrapper<C>>>,
     egraph: &'a mut E,
 }
 
-impl<'a, E: Egraph, C: CostSummary<Self, E::ENodeId> + Clone> FixedPointSolver<'a, E, C> {
+fn compute_costs<E: Egraph>(egraph: &mut E) -> HashMap<E::ENodeId, Cost> {
+    let mut costs = HashMap::default();
+    let mut node_vec = Vec::new();
+    let mut classes = Vec::new();
+    egraph.for_each_class(|x| classes.push(x.clone()));
+    for class in classes {
+        egraph.expand_class(&class, &mut node_vec);
+        for node in node_vec.drain(..) {
+            let cost = egraph.cost(&node);
+            costs.insert(node, cost);
+        }
+    }
+    costs
+}
+
+impl<'a, E: Egraph, C: CostSummary<E::ENodeId> + Clone + Eq> FixedPointSolver<'a, E, C> {
     pub(crate) fn new(
         egraph: &'a mut E,
         filter: &impl ENodeFilter<E::ENodeId>,
+        state: &mut C::State,
     ) -> FixedPointSolver<'a, E, C> {
         let mut class_guesses = HashMap::default();
         egraph.for_each_class(|class| {
@@ -39,25 +52,15 @@ impl<'a, E: Egraph, C: CostSummary<Self, E::ENodeId> + Clone> FixedPointSolver<'
                 NodeState {
                     node_id: None,
                     cost: Wrapper::default(),
-                    _marker: PhantomData,
                 },
             );
         });
-        let mut costs = HashMap::default();
-        let mut node_vec = Vec::new();
-        for (class, _) in class_guesses.iter() {
-            egraph.expand_class(class, &mut node_vec);
-            for node in node_vec.drain(..) {
-                let cost = egraph.cost(&node);
-                costs.insert(node, cost);
-            }
-        }
+
         let mut res = FixedPointSolver {
             class_guesses,
             egraph,
-            costs: Rc::new(costs),
         };
-        res.iterate(filter);
+        res.iterate(state, filter);
         res
     }
 
@@ -65,7 +68,7 @@ impl<'a, E: Egraph, C: CostSummary<Self, E::ENodeId> + Clone> FixedPointSolver<'
         &self.class_guesses
     }
 
-    fn iterate(&mut self, filter: &impl ENodeFilter<E::ENodeId>) {
+    fn iterate(&mut self, state: &mut C::State, filter: &impl ENodeFilter<E::ENodeId>) {
         let pool = Pool::<E>::default();
         let mut classes = pool.class_vec();
         classes.extend(self.class_guesses.keys().cloned());
@@ -73,15 +76,20 @@ impl<'a, E: Egraph, C: CostSummary<Self, E::ENodeId> + Clone> FixedPointSolver<'
         while changed {
             changed = false;
             for class in classes.iter() {
-                changed |= self.update_class(class, &pool, filter);
+                changed |= self.update_class(class, &pool, state, filter);
             }
         }
     }
 
-    fn compute_cost(&mut self, node: &E::ENodeId, pool: &Pool<E>) -> Wrapper<C> {
+    fn compute_cost(
+        &mut self,
+        node: &E::ENodeId,
+        pool: &Pool<E>,
+        state: &mut C::State,
+    ) -> Wrapper<C> {
         let mut classes = pool.class_vec();
         self.egraph.get_children(node, &mut classes);
-        let mut cost = Wrapper::<C>::singleton(self, node.clone(), self.egraph.cost(node));
+        let mut cost = Wrapper::<C>::singleton(state, node.clone());
         for class in classes.drain(..) {
             cost.combine(&self.class_guesses[&class].cost)
         }
@@ -92,6 +100,7 @@ impl<'a, E: Egraph, C: CostSummary<Self, E::ENodeId> + Clone> FixedPointSolver<'
         &mut self,
         class: &E::EClassId,
         pool: &Pool<E>,
+        state: &mut C::State,
         filter: &impl ENodeFilter<E::ENodeId>,
     ) -> bool {
         let mut changed = false;
@@ -100,7 +109,7 @@ impl<'a, E: Egraph, C: CostSummary<Self, E::ENodeId> + Clone> FixedPointSolver<'
         filter.filter(&mut nodes);
         let mut cur = self.class_guesses[class].clone();
         for node in nodes.drain(..) {
-            let cost = self.compute_cost(&node, pool);
+            let cost = self.compute_cost(&node, pool, state);
             changed |= cur.update(&node, &cost);
         }
 
@@ -112,23 +121,21 @@ impl<'a, E: Egraph, C: CostSummary<Self, E::ENodeId> + Clone> FixedPointSolver<'
     }
 }
 
-pub(crate) struct NodeState<N, C, S> {
+pub(crate) struct NodeState<N, C> {
     pub(crate) node_id: Option<N>,
     pub(crate) cost: C,
-    _marker: PhantomData<*const S>,
 }
 
-impl<S, N: Clone, C: Clone> Clone for NodeState<N, C, S> {
+impl<N: Clone, C: Clone> Clone for NodeState<N, C> {
     fn clone(&self) -> Self {
         NodeState {
             node_id: self.node_id.clone(),
             cost: self.cost.clone(),
-            _marker: PhantomData,
         }
     }
 }
 
-impl<S, N: Clone, C: CostSummary<S, N>> NodeState<N, C, S> {
+impl<N: Clone, C: CostSummary<N> + Eq> NodeState<N, C> {
     fn update(&mut self, node: &N, c: &C) -> bool {
         if self.cost.min_update(c) {
             self.node_id = Some(node.clone());
@@ -154,20 +161,14 @@ impl<C> Default for Wrapper<C> {
     }
 }
 
-impl<S, N, C: CostSummary<S, N> + Clone> CostSummary<S, N> for Wrapper<C> {
+impl<N, C: CostSummary<N> + Clone> CostSummary<N> for Wrapper<C> {
+    type State = C::State;
     fn combine(&mut self, other: &Self) {
         match (&mut self.0, &other.0) {
             (None, _) => {}
             (_, None) => *self = Self::default(),
             (Some(x), Some(y)) => x.combine(y),
         }
-    }
-
-    fn cost(&self) -> Cost {
-        self.0
-            .as_ref()
-            .map(|x| x.cost())
-            .unwrap_or_else(|| Cost::new(f64::INFINITY).unwrap())
     }
 
     fn min(&self, other: &Self) -> Self {
@@ -178,15 +179,12 @@ impl<S, N, C: CostSummary<S, N> + Clone> CostSummary<S, N> for Wrapper<C> {
         }
     }
 
-    fn singleton(state: &S, node: N, cost: Cost) -> Self {
-        Wrapper(Some(C::singleton(state, node, cost)))
+    fn singleton(state: &mut Self::State, node: N) -> Self {
+        Wrapper(Some(C::singleton(state, node)))
     }
 }
 
 // TODO: lots to update here with CostSummary:
-// * cost can probably just go away, in favor of a "State" arg, which is
-//   actually just passed in to the constructor. State can be an associated
-//   type.
 // * This State arg can be something like a ZddPool + intern table.
 // * CostSummary should implement Eq and min_update should be a basic thing that
 // does min, sees if it equals self, and if it doesn't, assigns and returns
@@ -199,37 +197,36 @@ impl<S, N, C: CostSummary<S, N> + Clone> CostSummary<S, N> for Wrapper<C> {
 ///
 /// CostSummaries are used to customize the "update rule" for greedy extraction
 /// algorithms.
-pub(crate) trait CostSummary<State, NodeId>: Sized {
-    fn cost(&self) -> Cost;
+pub(crate) trait CostSummary<NodeId>: Sized {
+    type State;
     fn min(&self, other: &Self) -> Self;
-    fn min_update(&mut self, other: &Self) -> bool {
+    fn min_update(&mut self, other: &Self) -> bool
+    where
+        Self: Eq,
+    {
         let res = self.min(other);
-        debug_assert!(res.cost() <= self.cost());
-        if res.cost() < self.cost() {
+        if &res != self {
             *self = res;
             true
         } else {
             false
         }
     }
-    fn singleton(state: &State, node: NodeId, cost: Cost) -> Self;
+    fn singleton(state: &mut Self::State, node: NodeId) -> Self;
     fn combine(&mut self, other: &Self);
 }
 
 #[derive(Default, Clone, PartialEq, Eq)]
 pub(crate) struct TreeCost(Cost);
 
-impl<State, NodeId> CostSummary<State, NodeId> for TreeCost {
-    fn cost(&self) -> Cost {
-        self.0
-    }
-
+impl<NodeId: Hash + Eq> CostSummary<NodeId> for TreeCost {
+    type State = HashMap<NodeId, Cost>;
     fn min(&self, other: &TreeCost) -> Self {
         TreeCost(cmp::min(self.0, other.0))
     }
 
-    fn singleton(_: &State, _: NodeId, cost: Cost) -> Self {
-        TreeCost(cost)
+    fn singleton(state: &mut Self::State, n: NodeId) -> Self {
+        TreeCost(state[&n])
     }
 
     fn combine(&mut self, other: &Self) {
@@ -276,66 +273,27 @@ impl<T: Hash + Eq + Clone> PartialEq for DagCost<T> {
 
 impl<T: Hash + Eq + Clone> Eq for DagCost<T> {}
 
-impl<'a, E: Egraph + 'a> CostSummary<FixedPointSolver<'a, E, Self>, E::ENodeId>
-    for DagCost<E::ENodeId>
-{
+impl<Node: Hash + Eq + Clone> CostSummary<Node> for DagCost<Node> {
+    type State = Rc<HashMap<Node, Cost>>;
+
     fn combine(&mut self, other: &Self) {
         self.set.union_agg(&other.set, |n| AddCost(self.costs[n]))
     }
 
-    fn cost(&self) -> Cost {
-        self.set.agg().0
-    }
-
     fn min(&self, other: &Self) -> Self {
-        if CostSummary::<FixedPointSolver<'a, E, Self>, E::ENodeId>::cost(self)
-            <= CostSummary::<FixedPointSolver<'a, E, Self>, E::ENodeId>::cost(other)
-        {
+        if self.set.agg().0 <= other.set.agg().0 {
             self.clone()
         } else {
             other.clone()
         }
     }
 
-    fn singleton(state: &FixedPointSolver<'a, E, Self>, node: E::ENodeId, _: Cost) -> Self {
-        let mut set = val_trie::HashSet::<E::ENodeId, AddCost>::default();
-        set.insert_agg(node, |n| AddCost(state.costs[n]));
+    fn singleton(state: &mut Self::State, node: Node) -> Self {
+        let mut set = val_trie::HashSet::<Node, AddCost>::default();
+        set.insert_agg(node, |n| AddCost(state[n]));
         DagCost {
             set,
-            costs: state.costs.clone(),
+            costs: state.clone(),
         }
     }
 }
-
-#[derive(Clone, PartialEq, Eq)]
-pub(crate) struct MentionedSet<T> {
-    set: val_trie::HashSet<T>,
-}
-
-impl<'a, E: Egraph + 'a> CostSummary<FixedPointSolver<'a, E, Self>, E::ENodeId>
-    for MentionedSet<E::ENodeId>
-{
-    fn combine(&mut self, other: &Self) {
-        self.set.union(&other.set)
-    }
-
-    fn cost(&self) -> Cost {
-        Cost::default()
-    }
-
-    fn min(&self, other: &Self) -> Self {
-        let mut set = self.set.clone();
-        set.union(&other.set);
-        MentionedSet { set }
-    }
-
-    fn singleton(_: &FixedPointSolver<'a, E, Self>, n: E::ENodeId, _: Cost) -> Self {
-        let mut set = val_trie::HashSet::<E::ENodeId>::default();
-        set.insert(n);
-        MentionedSet { set }
-    }
-}
-
-struct ZddNode(u32);
-
-// impl<'a, E:Egraph+'a> CostSummary<FixedPointSolver<'a, E, Self>, E::ENodeId> for Zdd<>
